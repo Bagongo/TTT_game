3fa14b7f3abe04fd08c39988af82333b5b36941b
Bagongo <akak@hotmail.it>
2017-03-25 14:21:51 +0100

    functioning AI implemented........

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.onload = function(){
 
-	var board = [["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]];
+	var gameBoard = [["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]];
 	var state = "continue";
 	var currPlayer = "X";
-	var totalMoves = 0;
+	var multiPlayer = false;
 
 	var display = document.getElementById("display");
 	var slots = document.getElementsByClassName("slot");
@@ -15,30 +15,70 @@ window.onload = function(){
 
 	function slotClicked()
 	{
-		if(board[this.dataset.posx][this.dataset.posy] !== "E" || state !== "continue")
+		var posX = this.dataset.posx;
+		var posY = this.dataset.posy;
+
+		if(gameBoard[posY][posX] !== "E" || state !== "continue")
 			return;
 
-		totalMoves++;
-		board[this.dataset.posx][this.dataset.posy] = currPlayer;
-		this.innerHTML = currPlayer;
-		evaluateState();
-
-		if(state !== "continue")
-			resolveGame();
-		else			
-			currPlayer = currPlayer === "X" ? "O" : "X";
+		makeMove(currPlayer, {x:posX, y:posY});
+		turnManager(false);
 	}
 
-	function evaluateState()
+	function makeMove(player, pos)
 	{
-		if(totalMoves >= 9)
+		for(var i=0; i <slots.length; i++)
+		{
+			if(slots[i].dataset.posx == pos.x && slots[i].dataset.posy == pos.y)
+				slots[i].innerHTML = player;
+		}	
+
+		gameBoard[pos.y][pos.x] = player;
+	}
+
+	function turnManager(ai)
+	{
+		evaluateState(gameBoard);
+
+		if(state !== "continue")
+			resolveGame(gameBoard);
+		else
+		{
+			if(multiPlayer)
+				currPlayer = currPlayer === "X" ? "O" : "X";
+			else
+			{
+				if(!ai)
+					AIMove();
+			}
+		}			
+	}
+
+	function evaluateState(board)
+	{
+		if(!emptySlotsLeft(board))
 			state = "draw";
 		
-		if (winningPositions().length > 0)
+		if (winningPositions(board).length > 0)
 			state = currPlayer + " wins!";
 	}
 
-	function winningPositions()
+
+	function emptySlotsLeft(board)
+	{
+		for(var i=0; i < board.length; i++)
+		{
+			for(var j=0; j < board[i].length; j++)
+			{
+				if(board[i][j] === "E")
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+	function winningPositions(board)
 	{
 		winPositions = []; //holds the positions of the slots forming the winning chain
 
@@ -71,13 +111,12 @@ window.onload = function(){
 	function resetState()
 	{
 		state = "continue";
-		totalMoves = 0;
 		currPlayer = "X";
 		display.innerHTML = "play!";
-		resetBoard();
+		resetBoard(gameBoard);
 	}
 
-	function resetBoard()
+	function resetBoard(board)
 	{
 		for(var i=0; i < board.length; i++)
 		{
@@ -87,6 +126,151 @@ window.onload = function(){
 
 		for(var x=0; x < slots.length; x++)
 			slots[x].innerHTML = "";
+	}
+
+	function AIMove()
+	{
+		var board = gameBoard.map(function(arr) {
+		     return arr.slice();
+		});
+
+		var move = findBestMove("O", board);
+
+		console.log(move);
+
+		makeMove("O", move);
+		turnManager(true);
+	}
+
+
+	var minimaxCounter = 0;
+	// This is the minimax function. It considers all
+	// the possible ways the game can go and returns
+	// the value of the board
+	function minimax(board, depth, isMax, seeds)
+	{
+
+		minimaxCounter++;
+		var best;	 
+	    // If Maximizer has won the game return his/her
+	    // evaluated score
+	    if (winningPositions(board).length > 0 && isMax)
+	        return -10;
+	 
+	    // If Minimizer has won the game return his/her
+	    // evaluated score
+	    if (winningPositions(board).length > 0 && !isMax)
+	        return 10;
+	 
+	    // If there are no more moves and no winner then
+	    // it is a tie
+	    if (!emptySlotsLeft(board))
+	        return 0;
+	 
+	    // If this maximizer's move
+	    if (isMax)
+	    {
+	        best = -1000;
+	 
+	        // Traverse all cells
+	        for (var i=0; i < board.length; i++)
+	        {
+	            for (var j=0; j < board[i].length; j++)
+	            {
+	                // Check if cell is empty
+	                if (board[i][j] === 'E')
+	                {
+	                    // Make the move
+	                    board[i][j] = seeds.ai;
+
+	                    // Call minimax recursively and choose
+	                    // the maximum value
+	                    best = Math.max(best, minimax(board, depth+1, !isMax, seeds));
+	 
+	                    // Undo the move
+	                    board[i][j] = 'E';
+	                }
+	            }
+	        }
+	        return best;
+	    }
+	 
+	    // If this minimizer's move
+	    else
+	    {
+	        best = 1000;
+	 
+	        // Traverse all cells
+	        for (var y=0; y < board.length; y++)
+	        {
+	            for (var x=0; x < board[y].length; x++)
+	            {
+	                // Check if cell is empty
+	                if (board[y][x] === 'E')
+	                {
+	                    // Make the move
+	                    board[y][x] = seeds.player;
+	 
+	                    // Call minimax recursively and choose
+	                    // the minimum value
+	                    best = Math.min(best, minimax(board, depth+1, !isMax, seeds));
+	 
+	                    // Undo the move
+	                    board[y][x] = 'E';
+	                }
+	            }
+	        }
+	        return best;
+	    }
+	}
+
+	// This will return the best possible move for the player
+	function findBestMove(seed, board)
+	{		
+		var seeds = {ai:seed, 
+					 player:(seed === "X") ? "0" : "X"};
+	    var bestVal = -1000;
+	    var bestMove = {};
+	 
+	    // Traverse all cells, evalutae minimax function for
+	    // all empty cells. And return the cell with optimal
+	    // value.
+	    for (var i=0; i < board.length; i++)
+	    {
+	        for (var j=0; j < board[i].length; j++)
+	        {
+	            // Check if celll is empty
+	            if (board[i][j] === 'E')
+	            {
+	                // Make the move
+	                board[i][j] = seeds.ai;
+	 
+	                // compute evaluation function for this
+	                // move.
+	                var moveVal = minimax(board, 0, false, seeds);
+
+	                console.log(moveVal + " " + i + " " + j);
+	 
+	                // Undo the move
+	                board[i][j] = 'E';
+	 
+	                // If the value of the current move is
+	                // more than the best value, then update
+	                // best/
+	                if (moveVal > bestVal)
+	                {
+	                    bestMove.x = j;
+	                    bestMove.y = i;
+	                    bestVal = moveVal;
+	                }
+	            }
+	        }
+	    }
+
+	    console.log(minimaxCounter);
+	    minimaxCounter = 0;
+	 
+	    return bestMove;
 	}
 
 };
