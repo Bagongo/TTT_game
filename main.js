29e4f45caecb2b0be1aa60d1023b514d969049aa
Bagongo <akak@hotmail.it>
2017-03-29 00:48:16 +0200

    Code commenting and bug fixes.........

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 window.onload = function(){
 
-	var gameBoard = [["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]];
-	var state = "continue";
-	var settings = {multiplayer: null, player1:null, player2:null};
-	var turn = null;
-	var startPlayer1 = true;
+//////////////////////////////////////////GLOBAL VARS AND LISTENERS///////////////////////////////////////////////////////////////////	
+
+	var gameBoard = [["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"]]; //representation of the game board, holds players' moves
+	var state = "continue"; //the current state of the game
+	var settings = {multiplayer: null, player1:null, player2:null}; //settings modifiable by user
+	var turn = null; //holds the seed of the player whose current turn is 
+	var startPlayer1 = true; //determines if player1 (or 2) will move first 
 
 	var display = document.getElementById("display");
 	var boardRef = document.getElementById("board");
-	var slots = document.getElementsByClassName("slot");
+	var slots = document.getElementsByClassName("slot"); //the board cells
 	for(var i=0; i < slots.length; i++)
 	{
 		slots[i].addEventListener("click", slotClicked);
-		slots[i].firstElementChild.addEventListener("transitionend", function(){this.classList.remove("flip");});
+		slots[i].firstElementChild.addEventListener("transitionend", function(){
+			this.classList.remove("flip");
+		});
 	}
 
+	//all of the game's buttons
 	var buttonBoxes = document.getElementsByClassName("button-box");
 	var buttons = document.getElementsByClassName("btn");
 	buttons[0].addEventListener("click", startSetup);
@@ -26,11 +31,15 @@ window.onload = function(){
 	buttons[6].addEventListener("click", startSetup);
 	buttons[7].addEventListener("click", resetGame);
 
-	function slotClicked()
+//////////////////////////////////////GAME INNER FUNCTIONING///////////////////////////////////////////////////
+
+	function slotClicked() 
 	{
+		//collecting the coordinates of the cell clicked
 		var posX = this.dataset.posx;
 		var posY = this.dataset.posy;
 
+		//return if cell not empty full or game's over 
 		if(gameBoard[posY][posX] !== "E" || state !== "continue")
 			return;
 
@@ -38,14 +47,16 @@ window.onload = function(){
 		turnManager(!settings.multiplayer);
 	}
 
-	function makeMove(player, pos)
+	function makeMove(player, pos) //actualize the move made
 	{
+		//finds the corresponding slot in the actual board and fills it
 		for(var i=0; i < slots.length; i++)
 		{
 			if(slots[i].dataset.posx == pos.x && slots[i].dataset.posy == pos.y)
 				slots[i].firstElementChild.innerHTML = player;
-		}	
-
+		}
+			
+		//fills the slot in the board array
 		gameBoard[pos.y][pos.x] = player;
 	}
 
@@ -55,12 +66,15 @@ window.onload = function(){
 
 		if(state === "begin")
 		{
+			//determines who's to start the game
 			turn = startPlayer1 === true ? settings.player1 : settings.player2;
-			display.innerHTML = turn + " to move";
+			//shows whose current turn is
+			display.innerHTML = turn + " to move"; 
 			state = "continue";	
 		}
 		else if(state === "continue")
 		{
+			//toggle current player for next turn
 			turn = turn === settings.player1 ? settings.player2 : settings.player1;
 			display.innerHTML = turn + " to move";
 
@@ -71,6 +85,7 @@ window.onload = function(){
 			resolveGame();
 	}
 
+	//checking if final state was reached
 	function evaluateState(board)
 	{
 		if(!emptySlotsLeft(board))
@@ -80,6 +95,7 @@ window.onload = function(){
 			state = turn + " wins!";
 	}
 
+	//detects if there are empty slots left
 	function emptySlotsLeft(board)
 	{
 		for(var i=0; i < board.length; i++)
@@ -94,7 +110,8 @@ window.onload = function(){
 		return false;
 	}
 
-	function winningPositions(board) //detects winning state and returns positions of the winning chain
+	//detects winning state and returns positions of the winning chain
+	function winningPositions(board)
 	{
 		winPositions = []; //holds positions of the slots forming the winning chain
 
@@ -119,22 +136,27 @@ window.onload = function(){
 		return winPositions;
 	}
 
+	//perform actions when game is over
 	function resolveGame()
 	{
 		var winPositions = winningPositions(gameBoard);
 
+		//search actual board for cells making up the winning chain
 		for(var i=0; i < winPositions.length; i++)
 		{
 			for(var j=0; j < winPositions[i].length; j++)
 				flipCell(winPositions[i][j]);	
 		}
 
+		//declares the winner
 		display.innerHTML = state;
 
+		//switches starting player for next game (multiplayer only)
 		if(settings.multiplayer)
 			startPlayer1 = !startPlayer1;
 	}
 
+	//triggers flip animation on cell
 	function flipCell(pos)
 	{
 		for(var i=0; i < slots.length; i++)
@@ -144,6 +166,7 @@ window.onload = function(){
 		}
 	}
 
+	//reset the state and boards for a new game
 	function resetGame()
 	{
 		resetBoard(gameBoard);
@@ -151,6 +174,7 @@ window.onload = function(){
 		turnManager(false);
 	}
 
+    //empties both virtual and actual board
 	function resetBoard(board)
 	{
 		for(var i=0; i < board.length; i++)
@@ -163,8 +187,11 @@ window.onload = function(){
 			slots[x].firstElementChild.innerHTML = "";
 	}
 
+	//AI's turn set of actions
 	function AIMove()
 	{
+		//makes a copy of the current game board
+		//for AI's ahead reasoning
 		var board = gameBoard.map(function(arr) {
 		     return arr.slice();
 		});
@@ -174,92 +201,86 @@ window.onload = function(){
 		turnManager(false);
 	}
 
-	// This is the minimax function. It considers all
-	// the possible ways the game can go and returns
-	// the value of the board
+///////////////////////////////////////AI SYSTEM/////////////////////////////////////////////////	
+
+	// Minimax - considers all the possible ways
+	// the game can go and returns a value
+	// for each playing path
 	function minimax(board, depth, isMax)
 	{
-		var best;	 
-	    // If Maximizer has won the game return his/her
-	    // evaluated score
+		var best;
+
+	    // if maximizer has won the game return pos value
 	    if (winningPositions(board).length > 0 && isMax)
 	        return -10;
 	 
-	    // If Minimizer has won the game return his/her
-	    // evaluated score
+	    // if minimizer has won the game return neg value 
 	    if (winningPositions(board).length > 0 && !isMax)
 	        return 10;
 	 
-	    // If there are no more moves and no winner then
-	    // it is a tie
+	    //if tie returns 0
 	    if (!emptySlotsLeft(board))
 	        return 0;
 	 
-	    // If this maximizer's move
+	    // If this is maximizer's move
 	    if (isMax)
 	    {
 	        best = -1000;
 	 
-	        // Traverse all cells
 	        for (var i=0; i < board.length; i++)
 	        {
 	            for (var j=0; j < board[i].length; j++)
 	            {
-	                // Check if cell is empty
 	                if (board[i][j] === 'E')
 	                {
-	                    // Make the move
+	                    // Make the hypothetical move
 	                    board[i][j] = settings.player2;
 
 	                    // Call minimax recursively and choose
-	                    // the maximum value
+	                    // and store the current best value
 	                    best = Math.max(best, minimax(board, depth+1, !isMax));
 	 
 	                    // Undo the move
+	                    // to clean the space for further recursions 
 	                    board[i][j] = 'E';
 	                }
 	            }
 	        }
+
 	        return best;
 	    }
 	 
-	    // If this minimizer's move
+	    // If this is minimizer's move works as maximizer but
+	    // with opposite parameters
 	    else
 	    {
 	        best = 1000;
 	 
-	        // Traverse all cells
 	        for (var y=0; y < board.length; y++)
 	        {
 	            for (var x=0; x < board[y].length; x++)
 	            {
-	                // Check if cell is empty
 	                if (board[y][x] === 'E')
 	                {
-	                    // Make the move
 	                    board[y][x] = settings.player1;
-	 
-	                    // Call minimax recursively and choose
-	                    // the minimum value
 	                    best = Math.min(best, minimax(board, depth+1, !isMax));
-	 
-	                    // Undo the move
-	                    board[y][x] = 'E';
+ 	                    board[y][x] = 'E';
 	                }
 	            }
 	        }
+
 	        return best;
 	    }
 	}
 
-	// This will return the best possible move for the player
+	// returns the best possible move for ai
 	function findBestMove(board)
 	{		
 	    var bestVal = -1000;
 	    var bestMove = {};
 	 
-	    // Traverse all cells, evalutae minimax function for
-	    // all empty cells. And return the cell with optimal
+	    // Traverses all cells, evaluates minimax function for
+	    // all empty cells. Then returns the cell with optimal
 	    // value.
 	    for (var i=0; i < board.length; i++)
 	    {
@@ -271,18 +292,16 @@ window.onload = function(){
 	                // Make the move
 	                board[i][j] = settings.player2;
 	 
-	                // compute evaluation function for this
-	                // move.
+	                // calculates the value for this move
 	                var moveVal = minimax(board, 0, false);
 	 
 	                // Undo the move
 	                board[i][j] = 'E';
 	 
-	                // If the value of the current move is
-	                // more than the best value, then update
-	                // best/
+					//updates best the best value found so far
 	                if (moveVal > bestVal)
 	                {
+	                	//stores coordinates of the current best move
 	                    bestMove.x = j;
 	                    bestMove.y = i;
 	                    bestVal = moveVal;
@@ -294,39 +313,15 @@ window.onload = function(){
 	    return bestMove;
 	}
 
+////////////////////////////////////GAME INTRO AND SETUP////////////////////////////////////////
+
+	//first step of the intro
 	function bootGame()
 	{
 		printToDisplay("Wanna play a game?", function(){showButtons([0, 1]);});
 	}
 
-	function startSetup()
-	{
-		hideAllButtons();
-		boardRef.style.display = "none";
-		buttonBoxes[0].classList.remove("no-display");
-		printToDisplay("Choose your opponent", function(){showButtons([2, 3]);});
-	}
-
-	function selectGameMode(value)
-	{
-		hideAllButtons(); 
-		settings.multiplayer = this.innerHTML === "Human";
-		printToDisplay("Choose your symbol", function(){showButtons([4, 5]);});
-	}
-
-	function selectSeed()
-	{
-		hideAllButtons();
-		buttonBoxes[0].classList.add("no-display");
-		settings.player1 = this.innerHTML;
-		settings.player2 = settings.player1 === "X" ? "O" : "X";
-		startPlayer1 = true;
-		boardRef.style.display = "flex";
-		buttonBoxes[1].classList.remove("no-display");		
-		showButtons([6, 7]);
-		resetGame();
-	}
-
+	//shows reference 
 	function showQuote()
 	{
 		hideAllButtons(); 
@@ -335,30 +330,70 @@ window.onload = function(){
 		});
 	}
 
+	//diplays 'choose vs' buttons 
+	function startSetup()
+	{
+		hideAllButtons();
+		boardRef.style.display = "none";
+		buttonBoxes[0].classList.remove("no-display");
+		printToDisplay("Choose your opponent", function(){showButtons([2, 3]);});
+	}
+
+	//detects and stores user opponent choice 
+	function selectGameMode(value)
+	{
+		hideAllButtons(); 
+		settings.multiplayer = this.innerHTML === "Human";
+		printToDisplay("Choose your symbol", function(){showButtons([4, 5]);});
+	}
+
+	//detects and stores user's seed choice and starts the game
+	function selectSeed()
+	{
+		hideAllButtons();
+		buttonBoxes[0].classList.add("no-display");
+		settings.player1 = this.innerHTML;
+		settings.player2 = settings.player1 === "X" ? "O" : "X";
+		startPlayer1 = true;
+		boardRef.style.display = "flex"; //reveals the actual board
+		buttonBoxes[1].classList.remove("no-display");		
+		showButtons([6, 7]); //reveals in-game buttons
+		resetGame(); //starts the game
+	}
+
+	//makes the setup buttons passed as params visible 
 	function showButtons(idxs)
 	{
 		buttons[idxs[0]].classList.remove("no-display");
 		buttons[idxs[1]].classList.remove("no-display");
 	}
 
+	//make all buttons non visible
 	function hideAllButtons()
 	{
 		for(var j=0; j < buttons.length; j++)
 			buttons[j].classList.add("no-display");
 	}
 
+	//gradually prints message to display as if somebody is typing
 	function printToDisplay(string, callback)
 	{
+		//empties the display
 		display.innerHTML = "";
 
+		//loops through each char of the message to be type out
 		for(i=0; i < string.length; i++)
 			doScaledTimeout(i);
 
+		//prints out each char one after the other 
+		//with brief delay in between
 		function doScaledTimeout(idx) 
 		{
 			setTimeout(function(){
 				display.innerHTML += string[idx];
 
+				//calls the function passed once the full
+				//message has been printed out
 				if(idx >= string.length - 1)
 					setTimeout(callback, 500);
 			}, idx * 100);
